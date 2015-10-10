32220c33e5cf9f9cacacd5147c028845ccc95f87
Chaim Sanders <csanders@sparsa.org>
2015-10-09 23:08:06 -0400

    Added some additional parsing

--- apache_http_modsecurity.c
+++ apache_http_modsecurity.c
@@ -14,6 +14,7 @@ static void register_hooks(apr_pool_t *pool);
 static int modsec_handler(request_rec *r);
 void *apache_http_modsecurity_create_loc_conf(apr_pool_t *mp, char *path);
 void *apache_http_modsecurity_create_main_conf(apr_pool_t* pool, server_rec* svr);
+static void* apache_http_modsecurity_merge_loc_conf(apr_pool_t* pool, void* parent, void* child);
 const char  *apache_http_modsecurity_set_remote_server(cmd_parms *cmd, void *cfg, const char *p1, const char *p2);
 
 typedef struct {
@@ -23,14 +24,12 @@ typedef struct {
 
 typedef struct {
     Rules *rules_set;
+    char *rules;
     char *rules_file;
     const char *rules_remote_server;
     const char *rules_remote_key;
-
     int enable;
     int id;
-
-    //Rules *rules_set;
 } apache_http_modsecurity_loc_conf_t;
 
 const char *apache_http_modsecurity_set_remote_server(cmd_parms *cmd, void *cfg, const char *p1, const char *p2)
@@ -48,34 +47,92 @@ const char *apache_http_modsecurity_set_remote_server(cmd_parms *cmd, void *cfg,
 
 static const command_rec module_directives[] =
 {
-    AP_INIT_FLAG("modsecurity", ap_set_flag_slot, NULL, OR_OPTIONS, "Turn ModSecurity on or off"),
-    AP_INIT_TAKE1("modsecurity_rules_file", ap_set_string_slot, NULL, OR_OPTIONS, "Load ModSecurity rules from a file"),
+    AP_INIT_FLAG("modsecurity", ap_set_flag_slot, (void*)APR_OFFSETOF(apache_http_modsecurity_loc_conf_t,enable), OR_OPTIONS, "The argument must be either 'On' or 'Off'"),
+    AP_INIT_TAKE1("modsecurity_rules_file", ap_set_string_slot, (void*)APR_OFFSETOF(apache_http_modsecurity_loc_conf_t,rules_file), OR_OPTIONS, "Load ModSecurity rules from a file"),
     AP_INIT_TAKE2("modsecurity_rules_remote", apache_http_modsecurity_set_remote_server, NULL, OR_OPTIONS, "Load ModSecurity rules from a remote server"),
-    AP_INIT_TAKE1("modsecurity_rules", ap_set_string_slot, NULL, OR_OPTIONS, "Specify ModSecurity rules inline"),
+    AP_INIT_TAKE1("modsecurity_rules", ap_set_string_slot, (void*)APR_OFFSETOF(apache_http_modsecurity_loc_conf_t,rules), OR_OPTIONS, "Please ensure that the arugment is specified correctly, including line continuations."),
     { NULL }
 };
 
 
-module AP_MODULE_DECLARE_DATA   security3_module  =
+module AP_MODULE_DECLARE_DATA security3_module  =
 {
     STANDARD20_MODULE_STUFF,
     apache_http_modsecurity_create_loc_conf, // Per-directory configuration
-    NULL,            // Merge handler for per-directory configurations
-    apache_http_modsecurity_create_main_conf, // Per-server configuration handler
+    apache_http_modsecurity_merge_loc_conf, // Merge handler for per-directory
+    apache_http_modsecurity_create_main_conf, // Per-server conf handler
     NULL,            // Merge handler for per-server configurations
     module_directives,
     register_hooks   
 };
 
+static void* apache_http_modsecurity_merge_loc_conf(apr_pool_t* pool, void* parent, void* child) {
+    fprintf(stderr,"Merge Request was called\n\n");
+    apache_http_modsecurity_loc_conf_t *p = NULL;
+    apache_http_modsecurity_loc_conf_t *c = NULL;
+    apache_http_modsecurity_loc_conf_t *conf = apr_palloc(pool, sizeof(apache_http_modsecurity_loc_conf_t));
+    p = parent;
+    c = child;
+    conf = p;
+    fprintf(stderr,"Rules set: '%p'\n", conf->rules_set);
+    if (p->rules_set != NULL)
+    {
+    	fprintf(stderr,"We have parental data");
+        fprintf(stderr,"Parent is not null, so we have to merge this configurations");
+        msc_rules_merge(c->rules_set, p->rules_set);
+    }
+    
+    if (c->rules_remote_server != NULL)
+    {
+        int res;
+        const char *error = NULL;
+        res = msc_rules_add_remote(c->rules_set, c->rules_remote_key, c->rules_remote_server, &error);
+        fprintf(stderr,"Loading rules from: '%s'", c->rules_remote_server);
+        if (res < 0) {
+            fprintf("Failed to load the rules from: '%s'  - reason: '%s'", c->rules_remote_server, error);
+
+            return strdup(error);
+        }
+        fprintf(stderr,"Loaded '%d' rules.", res);
+    }
+
+    if (c->rules_file != NULL)
+    {
+        int res;
+        const char *error = NULL;
+        res = msc_rules_add_file(c->rules_set, c->rules_set, &error);
+        fprintf(stderr,"Loading rules from: '%s'", c->rules_set);
+        if (res < 0) {
+            fprintf(stderr,"Failed to load the rules from: '%s' - reason: '%s'", c->rules_set, error);
+            return strdup(error);
+        }
+        fprintf(stderr,"Loaded '%d' rules.", res);
+    }
+
+    if (c->rules != NULL)
+    {
+        int res;
+        const char *error = NULL;
+        res = msc_rules_add(c->rules_set, c->rules, &error);
+        fprintf(stderr,"Loading rules: '%s'", c->rules);
+        if (res < 0) {
+            fprintf(stderr,"Failed to load the rules: '%s' - reason: '%s'", c->rules, error);
+            return strdup(error);
+        }
+    }
+    msc_rules_dump(c->rules_set);
+    return conf;
+}
+
 void *apache_http_modsecurity_create_main_conf(apr_pool_t* pool, server_rec* svr) {
     // This doesn't really do anything right now...
     apache_http_modsecurity_main_conf_t *config = apr_pcalloc(pool, sizeof(apache_http_modsecurity_main_conf_t));
     config->modsec = msc_init();
-    if(config->modsec != NULL){
+    if(config->modsec == NULL){
     	fprintf(stderr,"ModSecurity: We were unable to initalize the ModSecurity library, skipping hooks\n");
     	return NULL;
     }else{
-    	fprintf(stderr,"ModSecurity: Started Life");
+    	fprintf(stderr,"ModSecurity: Started Life\n");
     }
     msc_set_connector_info(config->modsec, "ModSecurity-apache v0.0.1-alpha");
     //msc_set_log_cb(config.modsec, ngx_http_modsecurity_log);
@@ -86,10 +143,10 @@ void *apache_http_modsecurity_create_main_conf(apr_pool_t* pool, server_rec* svr
 void *apache_http_modsecurity_create_loc_conf(apr_pool_t *mp, char *path){
 
     apache_http_modsecurity_loc_conf_t  *cf;
-    cf = (apache_http_modsecurity_loc_conf_t  *)
-        apr_palloc(mp, sizeof(apache_http_modsecurity_loc_conf_t));
+    cf = (apache_http_modsecurity_loc_conf_t  *)apr_palloc(mp, sizeof(apache_http_modsecurity_loc_conf_t));
     if (cf == NULL)
     {
+	fprintf(stderr,"Couldn't allocate space for our location specific configurations");
         return NULL;
     }
 
@@ -97,7 +154,6 @@ void *apache_http_modsecurity_create_loc_conf(apr_pool_t *mp, char *path){
     cf->rules_file = NULL;
     cf->rules_remote_server = NULL;
     cf->rules_remote_key = NULL;
-    // Figure out what to set this too
     cf->enable = 1;
     cf->id = 0;
     fprintf(stderr,"ModSecurity creating a location configuration\n");
