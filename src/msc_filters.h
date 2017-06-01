930baf43cba1a80013d709c4021dbffbc74b4861
Felipe Zimmerle <fcosta@trustwave.com>
2017-05-31 23:23:05 -0300

    Removes unused filters

--- src/msc_filters.h
+++ src/msc_filters.h
@@ -8,15 +8,4 @@
 
 #include "mod_security3.h"
 
-static void *FilterInCreateServerConfig(apr_pool_t *p, server_rec *s);
-static void *FilterOutCreateServerConfig(apr_pool_t *p, server_rec *s);
-static const char *FilterInEnable(cmd_parms *cmd, void *dummy, int arg);
-static const char *FilterOutEnable(cmd_parms *cmd, void *dummy, int arg);
-static int output_filter(ap_filter_t *f, apr_bucket_brigade *pbbIn);
-static int input_filter(ap_filter_t *f, apr_bucket_brigade *pbbOut, ap_input_mode_t eMode,
-                        apr_read_type_e eBlock, apr_off_t nBytes);
-
-static void InputFilter(request_rec *r);
-static void OutputFilter(request_rec *r);
-
 #endif  /* _SRC_MSC_FILTERS__ */
